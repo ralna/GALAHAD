1847c1242b71cc9b2dcc3bc7659a32f9c88dbfd2
nimgould <nick.gould@stfc.ac.uk>
2021-07-14 11:55:51 +0100

    added generic single and double precision include files

---
+++ include/galahad_single.h
@@ -0,0 +1,24 @@
+/*
+ * THIS VERSION: GALAHAD 3.3 - 27/01/2020 AT 10:30 GMT.
+ *
+ *-*-*-*-*-*-*-*-*-  GALAHAD C INTERFACE PRECISION  *-*-*-*-*-*-*-*-*-*-
+ *
+ *  Copyright reserved, Gould/Orban/Toint, for GALAHAD productions
+ *  Principal author: Jaroslav Fowkes
+ *
+ *  History -
+ *   currently in development
+ *
+ *  For full documentation, see
+ *   http://galahad.rl.ac.uk/galahad-www/specs.html
+ */
+
+// include guard
+#ifndef GALAHAD_PRECISION_H 
+#define GALAHAD_PRECISION_H
+
+typedef float real_wp_;  // working precision
+typedef float real_sp_;   // single precision
+
+// end include guard
+#endif
