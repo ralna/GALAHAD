d0f347a3bbcd108f2a96b92192be1329f16650f3
nimgould <nick.gould@stfc.ac.uk>
2022-03-22 14:30:06 +0000

    added missing C interface to fit

---
+++ src/fit/C/fit_ciface.f90
@@ -0,0 +1,303 @@
+! THIS VERSION: GALAHAD 4.0 - 2022-02-11 AT 13:45 GMT.
+
+!-*-*-*-*-*-*-*-  G A L A H A D _  F I T    C   I N T E R F A C E  -*-*-*-*-*-
+
+!  Copyright reserved, Gould/Orban/Toint, for GALAHAD productions
+!  Principal authors: Jaroslav Fowkes & Nick Gould
+
+!  History -
+!    originally released GALAHAD Version 4.0. January 28th 2022
+
+!  For full documentation, see
+!   http://galahad.rl.ac.uk/galahad-www/specs.html
+
+  MODULE GALAHAD_FIT_double_ciface
+    USE iso_c_binding
+    USE GALAHAD_common_ciface
+    USE GALAHAD_FIT_double, ONLY:                                              &
+        f_fit_control_type    => FIT_control_type,                             &
+        f_fit_inform_type    => FIT_inform_type,                               &
+        f_fit_full_data_type => FIT_full_data_type,                            &
+        f_fit_initialize     => FIT_initialize,                                &
+        f_fit_read_specfile  => FIT_read_specfile,                             &
+!       f_fit_import         => FIT_import,                                    &
+!       f_fit_reset_control  => FIT_reset_control,                             &
+!       f_fit_information    => FIT_information,                               &
+        f_fit_terminate      => FIT_terminate
+
+    IMPLICIT NONE
+
+!--------------------
+!   P r e c i s i o n
+!--------------------
+
+    INTEGER, PARAMETER :: wp = C_DOUBLE ! double precision
+    INTEGER, PARAMETER :: sp = C_FLOAT  ! single precision
+
+!-------------------------------------------------
+!  D e r i v e d   t y p e   d e f i n i t i o n s
+!-------------------------------------------------
+
+    TYPE, BIND( C ) :: fit_control_type
+      LOGICAL ( KIND = C_BOOL ) :: f_indexing
+      INTEGER ( KIND = C_INT ) :: error
+      INTEGER ( KIND = C_INT ) :: out
+      INTEGER ( KIND = C_INT ) :: print_level
+      LOGICAL ( KIND = C_BOOL ) :: space_critical
+      LOGICAL ( KIND = C_BOOL ) :: deallocate_error_fatal
+      CHARACTER ( KIND = C_CHAR ), DIMENSION( 31 ) :: prefix
+    END TYPE fit_control_type
+
+    TYPE, BIND( C ) :: fit_inform_type
+      INTEGER ( KIND = C_INT ) :: status
+      INTEGER ( KIND = C_INT ) :: alloc_status
+      CHARACTER ( KIND = C_CHAR ), DIMENSION( 81 ) :: bad_alloc
+    END TYPE fit_inform_type
+
+!----------------------
+!   P r o c e d u r e s
+!----------------------
+
+  CONTAINS
+
+!  copy C control parameters to fortran
+
+    SUBROUTINE copy_control_in( ccontrol, fcontrol, f_indexing ) 
+    TYPE ( fit_control_type ), INTENT( IN ) :: ccontrol
+    TYPE ( f_fit_control_type ), INTENT( OUT ) :: fcontrol
+    LOGICAL, optional, INTENT( OUT ) :: f_indexing
+    INTEGER :: i
+    
+    ! C or Fortran sparse matrix indexing
+    IF ( PRESENT( f_indexing ) ) f_indexing = ccontrol%f_indexing
+
+    ! Integers
+    fcontrol%error = ccontrol%error
+    fcontrol%out = ccontrol%out
+    fcontrol%print_level = ccontrol%print_level
+
+    ! Logicals
+    fcontrol%space_critical = ccontrol%space_critical
+    fcontrol%deallocate_error_fatal = ccontrol%deallocate_error_fatal
+
+    ! Strings
+    DO i = 1, LEN( fcontrol%prefix )
+      IF ( ccontrol%prefix( i ) == C_NULL_CHAR ) EXIT
+      fcontrol%prefix( i : i ) = ccontrol%prefix( i )
+    END DO
+    RETURN
+
+    END SUBROUTINE copy_control_in
+
+!  copy fortran control parameters to C
+
+    SUBROUTINE copy_control_out( fcontrol, ccontrol, f_indexing ) 
+    TYPE ( f_fit_control_type ), INTENT( IN ) :: fcontrol
+    TYPE ( fit_control_type ), INTENT( OUT ) :: ccontrol
+    LOGICAL, OPTIONAL, INTENT( IN ) :: f_indexing
+    INTEGER :: i, l
+    
+    ! C or Fortran sparse matrix indexing
+    IF ( PRESENT( f_indexing ) ) ccontrol%f_indexing = f_indexing
+
+    ! Integers
+    ccontrol%error = fcontrol%error
+    ccontrol%out = fcontrol%out
+    ccontrol%print_level = fcontrol%print_level
+
+    ! Logicals
+    ccontrol%space_critical = fcontrol%space_critical
+    ccontrol%deallocate_error_fatal = fcontrol%deallocate_error_fatal
+
+    ! Strings
+    l = LEN( fcontrol%prefix )
+    DO i = 1, l
+      ccontrol%prefix( i ) = fcontrol%prefix( i : i )
+    END DO
+    ccontrol%prefix( l + 1 ) = C_NULL_CHAR
+    RETURN
+
+    END SUBROUTINE copy_control_out
+
+!  copy C inform parameters to fortran
+
+    SUBROUTINE copy_inform_in( cinform, finform ) 
+    TYPE ( fit_inform_type ), INTENT( IN ) :: cinform
+    TYPE ( f_fit_inform_type ), INTENT( OUT ) :: finform
+    INTEGER :: i
+
+    ! Integers
+    finform%status = cinform%status
+    finform%alloc_status = cinform%alloc_status
+
+    ! Strings
+    DO i = 1, LEN( finform%bad_alloc )
+      IF ( cinform%bad_alloc( i ) == C_NULL_CHAR ) EXIT
+      finform%bad_alloc( i : i ) = cinform%bad_alloc( i )
+    END DO
+    RETURN
+
+    END SUBROUTINE copy_inform_in
+
+!  copy fortran inform parameters to C
+
+    SUBROUTINE copy_inform_out( finform, cinform ) 
+    TYPE ( f_fit_inform_type ), INTENT( IN ) :: finform
+    TYPE ( fit_inform_type ), INTENT( OUT ) :: cinform
+    INTEGER :: i, l
+
+    ! Integers
+    cinform%status = finform%status
+    cinform%alloc_status = finform%alloc_status
+
+    ! Strings
+    l = LEN( finform%bad_alloc )
+    DO i = 1, l
+      cinform%bad_alloc( i ) = finform%bad_alloc( i : i )
+    END DO
+    cinform%bad_alloc( l + 1 ) = C_NULL_CHAR
+    RETURN
+
+    END SUBROUTINE copy_inform_out
+
+  END MODULE GALAHAD_FIT_double_ciface
+
+!  -------------------------------------
+!  C interface to fortran fit_initialize
+!  -------------------------------------
+
+  SUBROUTINE fit_initialize( cdata, ccontrol, status ) BIND( C ) 
+  USE GALAHAD_FIT_double_ciface
+  IMPLICIT NONE
+
+!  dummy arguments
+
+  INTEGER ( KIND = C_INT ), INTENT( OUT ) :: status
+  TYPE ( C_PTR ), INTENT( OUT ) :: cdata ! data is a black-box
+  TYPE ( fit_control_type ), INTENT( OUT ) :: ccontrol
+
+!  local variables
+
+  TYPE ( f_fit_full_data_type ), POINTER :: fdata
+  TYPE ( f_fit_control_type ) :: fcontrol
+  TYPE ( f_fit_inform_type ) :: finform
+  LOGICAL :: f_indexing 
+
+!  allocate fdata
+
+  ALLOCATE( fdata ); cdata = C_LOC( fdata )
+
+!  initialize required fortran types
+
+  CALL f_fit_initialize( fdata, fcontrol, finform )
+  status = finform%status
+
+!  C sparse matrix indexing by default
+
+  f_indexing = .FALSE.
+  fdata%f_indexing = f_indexing
+
+!  copy control out 
+
+  CALL copy_control_out( fcontrol, ccontrol, f_indexing )
+  RETURN
+
+  END SUBROUTINE fit_initialize
+
+!  ----------------------------------------
+!  C interface to fortran fit_read_specfile
+!  ----------------------------------------
+
+  SUBROUTINE fit_read_specfile( ccontrol, cspecfile ) BIND( C )
+  USE GALAHAD_FIT_double_ciface
+  IMPLICIT NONE
+
+!  dummy arguments
+
+  TYPE ( fit_control_type ), INTENT( INOUT ) :: ccontrol
+  TYPE ( C_PTR ), INTENT( IN ), VALUE :: cspecfile
+
+!  local variables
+
+  TYPE ( f_fit_control_type ) :: fcontrol
+  CHARACTER ( KIND = C_CHAR, LEN = strlen( cspecfile ) ) :: fspecfile
+  LOGICAL :: f_indexing
+
+!  device unit number for specfile
+
+  INTEGER ( KIND = C_INT ), PARAMETER :: device = 10
+
+!  convert C string to Fortran string
+
+  fspecfile = cstr_to_fchar( cspecfile )
+
+!  copy control in
+
+  CALL copy_control_in( ccontrol, fcontrol, f_indexing )
+  
+!  open specfile for reading
+
+  OPEN( UNIT = device, FILE = fspecfile )
+  
+!  read control parameters from the specfile
+
+  CALL f_fit_read_specfile( fcontrol, device )
+
+!  close specfile
+
+  CLOSE( device )
+
+!  copy control out
+
+  CALL copy_control_out( fcontrol, ccontrol, f_indexing )
+  RETURN
+
+  END SUBROUTINE fit_read_specfile
+
+!  ------------------------------------
+!  C interface to fortran fit_terminate
+!  ------------------------------------
+
+  SUBROUTINE fit_terminate( cdata, ccontrol, cinform ) BIND( C ) 
+  USE GALAHAD_FIT_double_ciface
+  IMPLICIT NONE
+
+!  dummy arguments
+
+  TYPE ( C_PTR ), INTENT( INOUT ) :: cdata
+  TYPE ( fit_control_type ), INTENT( IN ) :: ccontrol
+  TYPE ( fit_inform_type ), INTENT( INOUT ) :: cinform
+
+!  local variables
+
+  TYPE ( f_fit_full_data_type ), pointer :: fdata
+  TYPE ( f_fit_control_type ) :: fcontrol
+  TYPE ( f_fit_inform_type ) :: finform
+  LOGICAL :: f_indexing
+
+!  copy control in
+
+  CALL copy_control_in( ccontrol, fcontrol, f_indexing )
+
+!  copy inform in
+
+  CALL copy_inform_in( cinform, finform )
+
+!  associate data pointer
+
+  CALL C_F_POINTER( cdata, fdata )
+
+!  deallocate workspace
+
+  CALL f_fit_terminate( fdata, fcontrol, finform )
+
+!  copy inform out
+
+  CALL copy_inform_out( finform, cinform )
+
+!  deallocate data
+
+  DEALLOCATE( fdata ); cdata = C_NULL_PTR 
+  RETURN
+
+  END SUBROUTINE fit_terminate
