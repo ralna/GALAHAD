122ea7bc85d0436e361f0395696b7546e8d5d2b5
nimgould <nick.gould@stfc.ac.uk>
2022-11-10 15:55:05 +0000

    updates to instructions so that valgrind can be used on test programs

---
+++ src/qpb/qpbs2.f90
@@ -0,0 +1,370 @@
+! THIS VERSION: GALAHAD 2.4 - 08/04/2010 AT 08:00 GMT.
+   PROGRAM GALAHAD_QPB_EXAMPLE
+   USE GALAHAD_QPB_double                            ! double precision version
+   USE GALAHAD_SYMBOLS
+   IMPLICIT NONE
+   INTEGER, PARAMETER :: wp = KIND( 1.0D+0 ) ! set precision
+   REAL ( KIND = wp ), PARAMETER :: infbnd = 10.0_wp ** 9
+   REAL ( KIND = wp ), PARAMETER :: infty = 10.0_wp * infbnd
+   TYPE ( QPT_problem_type ) :: p
+   TYPE ( QPB_data_type ) :: data
+   TYPE ( QPB_control_type ) :: control
+   TYPE ( QPB_inform_type ) :: info
+   INTEGER :: n, m, h_ne, a_ne, smt_stat, alloc_stat
+   INTEGER :: data_storage_type, i, status, scratch_out = 56
+   CHARACTER ( len = 1 ) :: st
+
+   n = 2 ; m = 1 ; h_ne = 3 ; a_ne = 2
+   ALLOCATE( p%G( n ), p%X_l( n ), p%X_u( n ) )
+   ALLOCATE( p%C( m ), p%C_l( m ), p%C_u( m ) )
+   ALLOCATE( p%X( n ), p%Y( m ), p%Z( n ) )
+   ALLOCATE( p%H%ptr( n + 1 ), p%A%ptr( m + 1 ) )
+
+   p%n = n ; p%m = m ; p%f = 0.05_wp
+   p%G = (/ 0.0_wp, 0.0_wp /)
+   p%C_l = (/ 1.0_wp /)
+   p%C_u = (/ 1.0_wp /)
+   p%X_l = (/ 0.0_wp, 0.0_wp /)
+   p%X_u = (/ 2.0_wp, 3.0_wp /)
+
+   p%new_problem_structure = .TRUE.
+   ALLOCATE( p%H%val( h_ne ), p%H%row( 0 ), p%H%col( h_ne ) )
+   ALLOCATE( p%A%val( a_ne ), p%A%row( 0 ), p%A%col( a_ne ) )
+   IF ( ALLOCATED( p%H%type ) ) DEALLOCATE( p%H%type )
+   CALL SMT_put( p%H%type, 'SPARSE_BY_ROWS', smt_stat )
+   p%H%col = (/ 1, 2, 1 /)
+   p%H%ptr = (/ 1, 2, 4 /)
+   IF ( ALLOCATED( p%A%type ) ) DEALLOCATE( p%A%type )
+   CALL SMT_put( p%A%type, 'SPARSE_BY_ROWS', smt_stat )
+   p%A%col = (/ 1, 2 /)
+   p%A%ptr = (/ 1, 3 /)
+go to 7
+   CALL QPB_initialize( data, control, info )
+   control%infinity = infbnd
+   control%restore_problem = 2
+! control%print_level = 1
+! control%SBLS_control%print_level = 1
+! control%LSQP_control%print_level = 1
+
+!  test with new and existing data
+
+   DO i = 0, 20
+!  DO i = 0, 0
+     IF ( i == 0 ) THEN
+       control%precon = 0
+     ELSE IF ( i == 1 ) THEN
+       control%SBLS_control%preconditioner = 1
+     ELSE IF ( i == 2 ) THEN
+       control%SBLS_control%preconditioner = 2
+     ELSE IF ( i == 3 ) THEN
+       control%SBLS_control%preconditioner = 3
+     ELSE IF ( i == 4 ) THEN
+       control%SBLS_control%preconditioner = 4
+     ELSE IF ( i == 5 ) THEN
+       control%SBLS_control%preconditioner = 5
+     ELSE IF ( i == 6 ) THEN
+       control%SBLS_control%preconditioner = 11
+     ELSE IF ( i == 7 ) THEN
+       control%SBLS_control%preconditioner = 12
+     ELSE IF ( i == 8 ) THEN
+       control%SBLS_control%preconditioner = - 1
+     ELSE IF ( i == 9 ) THEN
+       control%SBLS_control%preconditioner = - 2
+     ELSE IF ( i == 10 ) THEN
+       control%SBLS_control%factorization = - 1
+     ELSE IF ( i == 11 ) THEN
+       control%SBLS_control%factorization = 1
+     ELSE IF ( i == 12 ) THEN
+       control%SBLS_control%factorization = 1
+       control%SBLS_control%max_col = 0
+     ELSE IF ( i == 13 ) THEN
+       control%SBLS_control%factorization = 2
+       control%SBLS_control%preconditioner = 0
+     ELSE IF ( i == 14 ) THEN
+       control%SBLS_control%preconditioner = 1
+     ELSE IF ( i == 15 ) THEN
+       control%SBLS_control%preconditioner = 2
+     ELSE IF ( i == 16 ) THEN
+       control%SBLS_control%preconditioner = 3
+     ELSE IF ( i == 17 ) THEN
+       control%SBLS_control%preconditioner = 5
+     ELSE IF ( i == 18 ) THEN
+       control%center = .FALSE.
+     ELSE IF ( i == 19 ) THEN
+       control%primal = .TRUE.
+     ELSE IF ( i == 20 ) THEN
+       control%feasol = .FALSE.
+     END IF
+
+     p%H%val = (/ 1.0_wp, 1.0_wp, 0.25_wp /)
+     p%A%val = (/ 1.0_wp, 1.0_wp /)
+     p%X = 0.0_wp ; p%Y = 0.0_wp ; p%Z = 0.0_wp
+!    control%print_level = 1
+     CALL QPB_solve( p, data, control, info )
+!    write(6,"('x=', 2ES12.4)") p%X
+     IF ( info%status == 0 ) THEN
+       WRITE( 6, "( I2, ':', I6, ' iterations. Optimal objective value = ',    &
+     &       F6.1, ' status = ', I6 )" ) i, info%iter, info%obj, info%status
+     ELSE
+       WRITE( 6, "( I2, ': QPB_solve exit status = ', I6 ) " ) i, info%status
+     END IF
+   END DO
+   CALL QPB_terminate( data, control, info )
+
+!  case when there are no bounded variables
+
+7 continue
+write(6,*) ' at 7'
+   p%X_l = (/ - infty, - infty /)
+   p%X_u = (/ infty, infty /)
+   p%new_problem_structure = .TRUE.
+   IF ( ALLOCATED( p%H%type ) ) DEALLOCATE( p%H%type )
+   CALL SMT_put( p%H%type, 'SPARSE_BY_ROWS', smt_stat )
+   p%H%col = (/ 1, 2, 1 /)
+   p%H%ptr = (/ 1, 2, 4 /)
+   IF ( ALLOCATED( p%A%type ) ) DEALLOCATE( p%A%type )
+   CALL SMT_put( p%A%type, 'SPARSE_BY_ROWS', smt_stat )
+   p%A%col = (/ 1, 2 /)
+   p%A%ptr = (/ 1, 3 /)
+   CALL QPB_initialize( data, control, info )
+   control%infinity = infbnd
+   control%restore_problem = 2
+!  control%print_level = 4
+   DO i = 21, 21
+     p%H%val = (/ 1.0_wp, 1.0_wp, 0.0_wp /)
+     p%A%val = (/ 1.0_wp, 1.0_wp /)
+     p%X = 0.0_wp ; p%Y = 0.0_wp ; p%Z = 0.0_wp
+     CALL QPB_solve( p, data, control, info )
+!    write(6,"('x=', 2ES12.4)") p%X
+     IF ( info%status == 0 ) THEN
+       WRITE( 6, "( I2, ':', I6, ' iterations. Optimal objective value = ',    &
+     &       F6.1, ' status = ', I6 )" ) i, info%iter, info%obj, info%status
+     ELSE
+       WRITE( 6, "( I2, ': QPB_solve exit status = ', I6 ) " ) i, info%status
+     END IF
+   END DO
+   CALL QPB_terminate( data, control, info )
+go to 8
+!  case when there are no free variables ...
+
+   p%X_l = (/ 0.5_wp, 0.5_wp /)
+   p%X_u = (/ 0.5_wp, 0.5_wp /)
+   p%new_problem_structure = .TRUE.
+   IF ( ALLOCATED( p%H%type ) ) DEALLOCATE( p%H%type )
+   CALL SMT_put( p%H%type, 'SPARSE_BY_ROWS', smt_stat )
+   p%H%col = (/ 1, 2, 1 /)
+   p%H%ptr = (/ 1, 2, 4 /)
+   IF ( ALLOCATED( p%A%type ) ) DEALLOCATE( p%A%type )
+   CALL SMT_put( p%A%type, 'SPARSE_BY_ROWS', smt_stat )
+   p%A%col = (/ 1, 2 /)
+   p%A%ptr = (/ 1, 3 /)
+   CALL QPB_initialize( data, control, info )
+   control%infinity = infbnd
+   control%restore_problem = 2
+!  control%print_level = 11
+!  control%out = 6
+!  control%error = 6
+   DO i = 22, 22
+     p%H%val = (/ 1.0_wp, 1.0_wp, 0.0_wp /)
+     p%A%val = (/ 1.0_wp, 1.0_wp /)
+     p%X = 0.0_wp ; p%Y = 0.0_wp ; p%Z = 0.0_wp
+!    control%print_level = 1
+write(6,*) ' allocated c_u? ',  ALLOCATED( p%C_u )
+
+     CALL QPB_solve( p, data, control, info )
+write(6,*) ' allocated c_u? ',  ALLOCATED( p%C_u )
+!    write(6,"('x=', 2ES12.4)") p%X
+     IF ( info%status == 0 ) THEN
+       WRITE( 6, "( I2, ':', I6, ' iterations. Optimal objective value = ',    &
+     &       F6.1, ' status = ', I6 )" ) i, info%iter, info%obj, info%status
+     ELSE
+       WRITE( 6, "( I2, ': QPB_solve exit status = ', I6 ) " ) i, info%status
+     END IF
+   END DO
+   CALL QPB_terminate( data, control, info )
+
+8 continue
+write(6,*) ' here again'
+!  DEALLOCATE( p%G, p%X_l, p%X_u, p%C_l, p%C_u, stat = alloc_stat )
+ write(6,*) ' here '
+   DEALLOCATE( p%H%val, p%H%row, p%H%col )
+   DEALLOCATE( p%A%val, p%A%row, p%A%col )
+write(6,*) ' allocated c_u? ',  ALLOCATED( p%C_u )
+    DEALLOCATE( p%C_u )
+    DEALLOCATE( p%G )
+!write(6,*) ' here '
+    DEALLOCATE( p%X_l, p%X_u )
+!write(6,*) ' here 2'
+    DEALLOCATE( p%C_l )
+!write(6,*) ' here 3'
+!  DEALLOCATE( p%C_u )
+write(6,*) ' and here again'
+   DEALLOCATE( p%X, p%Y, p%Z, p%C )
+   DEALLOCATE( p%H%ptr, p%A%ptr )
+   DEALLOCATE( p%A%type, p%H%type )
+
+stop
+
+!  ============================
+!  full test of generic problem
+!  ============================
+
+9 continue
+   WRITE( 6, "( /, ' full test of generic problems ', / )" )
+
+   n = 14 ; m = 17 ; h_ne = 14 ; a_ne = 46
+   ALLOCATE( p%G( n ), p%X_l( n ), p%X_u( n ) )
+   ALLOCATE( p%C( m ), p%C_l( m ), p%C_u( m ) )
+   ALLOCATE( p%X( n ), p%Y( m ), p%Z( n ) )
+   ALLOCATE( p%H%ptr( n + 1 ), p%A%ptr( m + 1 ) )
+   ALLOCATE( p%H%val( h_ne ), p%H%row( h_ne ), p%H%col( h_ne ) )
+   ALLOCATE( p%A%val( a_ne ), p%A%row( a_ne ), p%A%col( a_ne ) )
+   p%new_problem_structure = .TRUE.
+   IF ( ALLOCATED( p%H%type ) ) DEALLOCATE( p%H%type )
+   CALL SMT_put( p%H%type, 'COORDINATE', smt_stat )
+   IF ( ALLOCATED( p%A%type ) ) DEALLOCATE( p%A%type )
+   CALL SMT_put( p%A%type, 'COORDINATE', smt_stat )
+   p%n = n ; p%m = m ; p%H%ne = h_ne ; p%A%ne = a_ne
+   p%f = 1.0_wp
+   p%G = (/ 0.0_wp, 2.0_wp, 0.0_wp, 0.0_wp, 2.0_wp, 0.0_wp, 2.0_wp,            &
+            0.0_wp, 2.0_wp, 0.0_wp, 0.0_wp, 2.0_wp, 0.0_wp, 2.0_wp /)
+   p%C_l = (/ 4.0_wp, 2.0_wp, 6.0_wp, - infty, - infty,                        &
+              4.0_wp, 2.0_wp, 6.0_wp, - infty, - infty,                        &
+              - 10.0_wp, - 10.0_wp, - 10.0_wp, - 10.0_wp,                      &
+              - 10.0_wp, - 10.0_wp, - 10.0_wp /)
+   p%C_u = (/ 4.0_wp, infty, 10.0_wp, 2.0_wp, infty,                           &
+              4.0_wp, infty, 10.0_wp, 2.0_wp, infty,                           &
+              10.0_wp, 10.0_wp, 10.0_wp, 10.0_wp,                              &
+              10.0_wp, 10.0_wp, 10.0_wp /)
+   p%X_l = (/ 1.0_wp, 0.0_wp, 1.0_wp, 2.0_wp, - infty, - infty, - infty,       &
+              1.0_wp, 0.0_wp, 1.0_wp, 2.0_wp, - infty, - infty, - infty /)
+   p%X_u = (/ 1.0_wp, infty, infty, 3.0_wp, 4.0_wp, 0.0_wp, infty,             &
+              1.0_wp, infty, infty, 3.0_wp, 4.0_wp, 0.0_wp, infty /)
+   p%H%val = (/ 1.0_wp, 1.0_wp, 2.0_wp, 2.0_wp, 3.0_wp, 3.0_wp,                &
+                4.0_wp, 4.0_wp, 5.0_wp, 5.0_wp, 6.0_wp, 6.0_wp,                &
+                7.0_wp, 7.0_wp /)
+   p%H%row = (/ 1, 8, 2, 9, 3, 10, 4, 11, 5, 12, 6, 13, 7, 14 /)
+   p%H%col = (/ 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 /)
+   p%A%val = (/ 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp,                        &
+                1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp,                &
+                1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp,                        &
+                1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp,                        &
+                1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp,                &
+                1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp,                        &
+                1.0_wp, - 1.0_wp, 1.0_wp, - 1.0_wp, 1.0_wp, - 1.0_wp,          &
+                1.0_wp, - 1.0_wp, 1.0_wp, - 1.0_wp, 1.0_wp, - 1.0_wp,          &
+                1.0_wp, - 1.0_wp /)
+   p%A%row = (/ 1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 5, 5, 5,                &
+                6, 6, 6, 7, 7, 8, 8, 8, 8, 8, 8, 9, 9, 10, 10, 10,             &
+                11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17 /)
+   p%A%col = (/ 1, 3, 5, 1, 2, 1, 2, 3, 4, 5, 6, 5, 6, 2, 4, 6,                &
+                8, 10, 12, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 9, 11, 13,      &
+                1, 8, 2, 9, 3, 10, 4, 11, 5, 12, 6, 13, 7, 14 /)
+
+   CALL QPB_initialize( data, control, info )
+   control%infinity = infbnd
+   control%restore_problem = 1
+   control%print_level = 101
+   control%itref_max = 3
+   control%out = scratch_out
+   control%error = scratch_out
+   control%out = 6
+   control%error = 6
+   p%X = 0.0_wp ; p%Y = 0.0_wp ; p%Z = 0.0_wp
+!stop
+!  OPEN( UNIT = scratch_out, STATUS = 'SCRATCH', ERR = 10, iostat = i )
+!  OPEN( UNIT = scratch_out, STATUS = 'scratch', ERR = 10, iostat = i )
+10 continue
+   write(6,*) ' iostat = ', i
+write(6,*) ' after open'
+!stop
+   CALL QPB_solve( p, data, control, info )
+   CLOSE( UNIT = scratch_out )
+   IF ( info%status == 0 ) THEN
+       WRITE( 6, "( I2, ':', I6, ' iterations. Optimal objective value = ',    &
+     &       F6.1, ' status = ', I6 )" ) 1, info%iter, info%obj, info%status
+   ELSE
+     WRITE( 6, "( I2, ': QPB_solve exit status = ', I6 ) " ) 1, info%status
+   END IF
+   CALL QPB_terminate( data, control, info )
+   DEALLOCATE( p%H%val, p%H%row, p%H%col )
+   DEALLOCATE( p%A%val, p%A%row, p%A%col )
+   DEALLOCATE( p%H%ptr, p%A%ptr )
+   DEALLOCATE( p%A%type, p%H%type )
+   DEALLOCATE( p%G, p%X_l, p%X_u, p%C_l, p%C_u )
+   DEALLOCATE( p%X, p%Y, p%Z, p%C )
+
+!  Second problem
+
+   n = 14 ; m = 17 ; h_ne = 14 ; a_ne = 46
+   ALLOCATE( p%G( n ), p%X_l( n ), p%X_u( n ) )
+   ALLOCATE( p%C( m ), p%C_l( m ), p%C_u( m ) )
+   ALLOCATE( p%X( n ), p%Y( m ), p%Z( n ) )
+   ALLOCATE( p%H%ptr( n + 1 ), p%A%ptr( m + 1 ) )
+   ALLOCATE( p%H%val( h_ne ), p%H%row( h_ne ), p%H%col( h_ne ) )
+   ALLOCATE( p%A%val( a_ne ), p%A%row( a_ne ), p%A%col( a_ne ) )
+   p%new_problem_structure = .TRUE.
+   IF ( ALLOCATED( p%H%type ) ) DEALLOCATE( p%H%type )
+   CALL SMT_put( p%H%type, 'COORDINATE', smt_stat )
+   IF ( ALLOCATED( p%A%type ) ) DEALLOCATE( p%A%type )
+   CALL SMT_put( p%A%type, 'COORDINATE', smt_stat )
+   p%n = n ; p%m = m ; p%H%ne = h_ne ; p%A%ne = a_ne
+   p%f = 1.0_wp
+   p%G = (/ 0.0_wp, 2.0_wp, 0.0_wp, 0.0_wp, 2.0_wp, 0.0_wp, 2.0_wp,            &
+            0.0_wp, 2.0_wp, 0.0_wp, 0.0_wp, 2.0_wp, 0.0_wp, 2.0_wp /)
+   p%C_l = (/ 4.0_wp, 2.0_wp, 6.0_wp, - infty, - infty,                        &
+              4.0_wp, 2.0_wp, 6.0_wp, - infty, - infty,                        &
+              - 10.0_wp, - 10.0_wp, - 10.0_wp, - 10.0_wp,                      &
+              - 10.0_wp, - 10.0_wp, - 10.0_wp /)
+   p%C_u = (/ 4.0_wp, infty, 10.0_wp, 2.0_wp, infty,                           &
+              4.0_wp, infty, 10.0_wp, 2.0_wp, infty,                           &
+              10.0_wp, 10.0_wp, 10.0_wp, 10.0_wp,                              &
+              10.0_wp, 10.0_wp, 10.0_wp /)
+   p%X_l = (/ 1.0_wp, 0.0_wp, 1.0_wp, 2.0_wp, - infty, - infty, - infty,       &
+              1.0_wp, 0.0_wp, 1.0_wp, 2.0_wp, - infty, - infty, - infty /)
+   p%X_u = (/ 1.0_wp, infty, infty, 3.0_wp, 4.0_wp, 0.0_wp, infty,             &
+              1.0_wp, infty, infty, 3.0_wp, 4.0_wp, 0.0_wp, infty /)
+   p%H%val = (/ 1.0_wp, 1.0_wp, 2.0_wp, 2.0_wp, 3.0_wp, 3.0_wp,                &
+                4.0_wp, 4.0_wp, 5.0_wp, 5.0_wp, 6.0_wp, 6.0_wp,                &
+                7.0_wp, 7.0_wp /)
+   p%H%row = (/ 1, 8, 2, 9, 3, 10, 4, 11, 5, 12, 6, 13, 7, 14 /)
+   p%H%col = (/ 1, 8, 2, 9, 3, 10, 4, 11, 5, 12, 6, 13, 7, 14 /)
+   p%A%val = (/ 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp,                        &
+                1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp,                &
+                1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp,                        &
+                1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp,                        &
+                1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp,                &
+                1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp, 1.0_wp,                        &
+                1.0_wp, - 1.0_wp, 1.0_wp, - 1.0_wp, 1.0_wp, - 1.0_wp,          &
+                1.0_wp, - 1.0_wp, 1.0_wp, - 1.0_wp, 1.0_wp, - 1.0_wp,          &
+                1.0_wp, - 1.0_wp /)
+   p%A%row = (/ 1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 5, 5, 5,                &
+                6, 6, 6, 7, 7, 8, 8, 8, 8, 8, 8, 9, 9, 10, 10, 10,             &
+                11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17 /)
+   p%A%col = (/ 1, 3, 5, 1, 2, 1, 2, 3, 4, 5, 6, 5, 6, 2, 4, 6,                &
+                8, 10, 12, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 9, 11, 13,      &
+                1, 8, 2, 9, 3, 10, 4, 11, 5, 12, 6, 13, 7, 14 /)
+
+   CALL QPB_initialize( data, control, info )
+   control%infinity = infbnd
+   control%restore_problem = 0
+   control%treat_zero_bounds_as_general = .TRUE.
+   p%X = 0.0_wp ; p%Y = 0.0_wp ; p%Z = 0.0_wp
+   CALL QPB_solve( p, data, control, info )
+   IF ( info%status == 0 ) THEN
+       WRITE( 6, "( I2, ':', I6, ' iterations. Optimal objective value = ',    &
+     &       F6.1, ' status = ', I6 )" ) 2, info%iter, info%obj, info%status
+   ELSE
+     WRITE( 6, "( I2, ': QPB_solve exit status = ', I6 ) " ) 2, info%status
+   END IF
+   CALL QPB_terminate( data, control, info )
+   DEALLOCATE( p%H%val, p%H%row, p%H%col )
+   DEALLOCATE( p%A%val, p%A%row, p%A%col )
+   DEALLOCATE( p%H%ptr, p%A%ptr )
+   DEALLOCATE( p%G, p%X_l, p%X_u, p%C_l, p%C_u )
+   DEALLOCATE( p%X, p%Y, p%Z, p%C )
+   DEALLOCATE( p%A%type, p%H%type )
+   IF ( ALLOCATED( p%WEIGHT ) ) DEALLOCATE( p%WEIGHT )
+   IF ( ALLOCATED( p%X0 ) ) DEALLOCATE( p%X0 )
+
+   END PROGRAM GALAHAD_QPB_EXAMPLE
