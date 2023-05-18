f5805cc7469ea3291be68d2efcb987328cf05531
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-05-18 08:39:58 -0400

    Add galahad_blas.F90

---
+++ src/external/blas/galahad_blas.F90
@@ -0,0 +1,34 @@
+! THIS VERSION: GALAHAD 5.0 - 2023-05-18 AT 14:15 GMT.
+
+include "v3.11/snrm2.f90"
+include "v3.11/dnrm2.f90"
+
+include "v3.11/srotg.f90"
+include "v3.11/drotg.f90"
+
+include "v3.11/srot.f"
+include "v3.11/drot.f"
+
+include "v3.11/sswap.f"
+include "v3.11/dswap.f"
+
+include "v3.11/sscal.f"
+include "v3.11/dscal.f"
+
+include "v3.11/isamax.f"
+include "v3.11/idamax.f"
+
+include "v3.11/strsv.f"
+include "v3.11/dtrsv.f"
+
+include "v3.11/strsm.f"
+include "v3.11/dtrsm.f"
+
+include "v3.11/stbsv.f"
+include "v3.11/dtbsv.f"
+
+include "v3.11/sgemv.f"
+include "v3.11/dgemv.f"
+
+include "v3.11/sger.f"
+include "v3.11/dger.f"
