3cbe654183141b5e41eea1a83d543a090e134dd2
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-05-19 02:15:11 -0400

    Compile GALAHAD with reference LAPACK v3.11

---
+++ src/external/lapack/galahad_lapack1.f
@@ -0,0 +1,371 @@
+! THIS VERSION: GALAHAD 5.0 - 2023-05-18 AT 14:15 GMT.
+
+include "ilaenv.f"
+include "iparmq.f"
+include "iladlr.f"
+include "ilaslc.f"
+include "ilaslr.f"
+include "iladlc.f"
+
+include "slaic1.f"
+include "dlaic1.f"
+
+include "stzrzf.f"
+include "dtzrzf.f"
+
+include "sgeqrf.f"
+include "dgeqrf.f"
+
+include "sgetrf.f"
+include "dgetrf.f"
+
+include "sgetrf2.f"
+include "dgetrf2.f"
+
+include "sgetrs.f"
+include "dgetrs.f"
+
+include "sgels.f"
+include "dgels.f"
+
+include "sgelsy.f"
+include "dgelsy.f"
+
+include "sgelss.f"
+include "dgelss.f"
+
+include "sgelsd.f"
+include "dgelsd.f"
+
+include "sgesvd.f"
+include "dgesvd.f"
+
+include "spttrf.f"
+include "dpttrf.f"
+
+include "spotrf.f"
+include "dpotrf.f"
+
+include "spotrf2.f"
+include "dpotrf2.f"
+
+include "spotrs.f"
+include "dpotrs.f"
+
+include "ssytrf.f"
+include "dsytrf.f"
+
+include "ssytrs.f"
+include "dsytrs.f"
+
+include "spbtrf.f"
+include "dpbtrf.f"
+
+include "spbtf2.f"
+include "dpbtf2.f"
+
+include "spbtrs.f"
+include "dpbtrs.f"
+
+include "ssyev.f"
+include "dsyev.f"
+
+include "ssygv.f"
+include "dsygv.f"
+
+include "shseqr.f"
+include "dhseqr.f"
+
+include "ssterf.f"
+include "dsterf.f"
+
+include "slaev2.f"
+include "dlaev2.f"
+
+include "ssygst.f"
+include "dsygst.f"
+
+include "slasyf.f"
+include "dlasyf.f"
+
+include "ssytf2.f"
+include "dsytf2.f"
+
+include "spotf2.f"
+include "dpotf2.f"
+
+include "slaqr0.f"
+include "dlaqr0.f"
+
+include "slahqr.f"
+include "dlahqr.f"
+
+include "sisnan.f"
+include "disnan.f"
+
+include "slaisnan.f"
+include "dlaisnan.f"
+
+include "slacpy.f"
+include "dlacpy.f"
+
+include "slaset.f"
+include "dlaset.f"
+
+include "sbdsqr.f"
+include "dbdsqr.f"
+
+include "slascl.f"
+include "dlascl.f"
+
+include "sorgbr.f"
+include "dorgbr.f"
+
+include "sormbr.f"
+include "dormbr.f"
+
+include "slaqr1.f"
+include "dlaqr1.f"
+
+include "slaqr2.f"
+include "dlaqr2.f"
+
+include "slaqr3.f"
+include "dlaqr3.f"
+
+include "slaqr4.f"
+include "dlaqr4.f"
+
+include "slaqr5.f"
+include "dlaqr5.f"
+
+include "slanv2.f"
+include "dlanv2.f"
+
+include "slabad.f"
+include "dlabad.f"
+
+include "sorgqr.f"
+include "dorgqr.f"
+
+include "slamch.f"
+include "dlamch.f"
+
+include "sgelqf.f"
+include "dgelqf.f"
+
+include "sorglq.f"
+include "dorglq.f"
+
+include "slange.f"
+include "dlange.f"
+
+include "sgebrd.f"
+include "dgebrd.f"
+
+include "slalsd.f"
+include "dlalsd.f"
+
+include "sormlq.f"
+include "dormlq.f"
+
+include "sormqr.f"
+include "dormqr.f"
+
+include "srscl.f"
+include "drscl.f"
+
+include "sormrz.f"
+include "dormrz.f"
+
+include "strtrs.f"
+include "dtrtrs.f"
+
+include "slaswp.f"
+include "dlaswp.f"
+
+include "sgeqp3.f"
+include "dgeqp3.f"
+
+include "sormr3.f"
+include "dormr3.f"
+
+include "slaqp2.f"
+include "dlaqp2.f"
+
+include "slarzt.f"
+include "dlarzt.f"
+
+include "slarf.f"
+include "dlarf.f"
+
+include "slarzb.f"
+include "dlarzb.f"
+
+include "slarfg.f"
+include "dlarfg.f"
+
+include "slapy2.f"
+include "dlapy2.f"
+
+include "slarz.f"
+include "dlarz.f"
+
+include "slaqps.f"
+include "dlaqps.f"
+
+include "slatrz.f"
+include "dlatrz.f"
+
+include "slarfb.f"
+include "dlarfb.f"
+
+include "slarft.f"
+include "dlarft.f"
+
+include "sorml2.f"
+include "dorml2.f"
+
+include "sorm2r.f"
+include "dorm2r.f"
+
+include "slasrt.f"
+include "dlasrt.f"
+
+include "slasdq.f"
+include "dlasdq.f"
+
+include "slasda.f"
+include "dlasda.f"
+
+include "slasr.f"
+include "dlasr.f"
+
+include "slalsa.f"
+include "dlalsa.f"
+
+include "sgehrd.f"
+include "dgehrd.f"
+
+include "sormhr.f"
+include "dormhr.f"
+
+include "strexc.f"
+include "dtrexc.f"
+
+include "slahr2.f"
+include "dlahr2.f"
+
+include "slansy.f"
+include "dlansy.f"
+
+include "ssteqr.f"
+include "dsteqr.f"
+
+include "sgelq2.f"
+include "dgelq2.f"
+
+include "sorg2r.f"
+include "dorg2r.f"
+
+include "sgeqr2.f"
+include "dgeqr2.f"
+
+include "sorgl2.f"
+include "dorgl2.f"
+
+include "slals0.f"
+include "dlals0.f"
+
+include "slamrg.f"
+include "dlamrg.f"
+
+include "sgebd2.f"
+include "dgebd2.f"
+
+include "slasdt.f"
+include "dlasdt.f"
+
+include "slabrd.f"
+include "dlabrd.f"
+
+include "slasd4.f"
+include "dlasd4.f"
+
+include "slasd5.f"
+include "dlasd5.f"
+
+include "slasd6.f"
+include "dlasd6.f"
+
+include "slasd7.f"
+include "dlasd7.f"
+
+include "slasd8.f"
+include "dlasd8.f"
+
+include "slaed6.f"
+include "dlaed6.f"
+
+include "slanst.f"
+include "dlanst.f"
+
+include "slas2.f"
+include "dlas2.f"
+
+include "slae2.f"
+include "dlae2.f"
+
+include "slasq1.f"
+include "dlasq1.f"
+
+include "slasq2.f"
+include "dlasq2.f"
+
+include "slasq3.f"
+include "dlasq3.f"
+
+include "slasq4.f"
+include "dlasq4.f"
+
+include "slasq5.f"
+include "dlasq5.f"
+
+include "slasq6.f"
+include "dlasq6.f"
+
+include "ssygs2.f"
+include "dsygs2.f"
+
+include "slasv2.f"
+include "dlasv2.f"
+
+include "slaexc.f"
+include "dlaexc.f"
+
+include "sorgtr.f"
+include "dorgtr.f"
+
+include "slarfx.f"
+include "dlarfx.f"
+
+include "sgehd2.f"
+include "dgehd2.f"
+
+include "slasy2.f"
+include "dlasy2.f"
+
+include "ssytrd.f"
+include "dsytrd.f"
+
+include "ssytd2.f"
+include "dsytd2.f"
+
+include "sorgql.f"
+include "dorgql.f"
+
+include "slatrd.f"
+include "dlatrd.f"
+
+include "sorg2l.f"
+include "dorg2l.f"
---
+++ src/external/lapack/galahad_lapack2.F90
@@ -0,0 +1,9 @@
+! THIS VERSION: GALAHAD 5.0 - 2023-05-18 AT 14:15 GMT.
+
+include "la_constants.f90"
+
+include "slartg.f90"
+include "dlartg.f90"
+
+include "slassq.f90"
+include "dlassq.f90"
