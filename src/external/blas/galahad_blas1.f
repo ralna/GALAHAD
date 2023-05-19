c13933b76887ef066297e41e886281c4fb3cfe49
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-05-19 02:14:58 -0400

    Compile GALAHAD with reference BLAS v3.11

---
+++ src/external/blas/galahad_blas1.f
@@ -0,0 +1,70 @@
+! THIS VERSION: GALAHAD 5.0 - 2023-05-18 AT 14:15 GMT.
+
+include "xerbla.f"
+include "lsame.f"
+
+include "srot.f"
+include "drot.f"
+
+include "sswap.f"
+include "dswap.f"
+
+include "sscal.f"
+include "dscal.f"
+
+include "isamax.f"
+include "idamax.f"
+
+include "strsv.f"
+include "dtrsv.f"
+
+include "strsm.f"
+include "dtrsm.f"
+
+C include "strmv.f"
+C include "dtrmv.f"
+
+include "strmm.f"
+include "dtrmm.f"
+
+include "stbsv.f"
+include "dtbsv.f"
+
+include "sgemv.f"
+include "dgemv.f"
+
+include "sgemm.f"
+include "dgemm.f"
+
+include "sger.f"
+include "dger.f"
+
+include "ssyrk.f"
+include "dsyrk.f"
+
+include "scopy.f"
+include "dcopy.f"
+
+include "saxpy.f"
+include "daxpy.f"
+
+include "ssyr.f"
+include "dsyr.f"
+
+include "sdot.f"
+include "ddot.f"
+
+include "ssymv.f"
+include "dsymv.f"
+
+include "ssymm.f"
+include "dsymm.f"
+
+include "ssyr2.f"
+include "dsyr2.f"
+
+include "ssyr2k.f"
+include "dsyr2k.f"
+
+include "strmv.f"
+include "dtrmv.f"
---
+++ src/external/blas/galahad_blas2.F90
@@ -0,0 +1,7 @@
+! THIS VERSION: GALAHAD 5.0 - 2023-05-18 AT 14:15 GMT.
+
+include "snrm2.f90"
+include "dnrm2.f90"
+
+include "srotg.f90"
+include "drotg.f90"
