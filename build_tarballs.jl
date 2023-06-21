a5677b16260670485102381ee7e4b3d0c713ac8d
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-06-21 02:11:05 -0400

    push local modifications

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -53,4 +53,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="v1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="v1.9")
