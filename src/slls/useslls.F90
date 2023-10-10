23e4e6bd5a9b8b6d5f8aba0d7d391c51aaad649d
Nick Gould <dalekopera@gmail.com>
2023-10-10 15:43:50 +0100

    another blunder is useslls fixed

--- src/slls/useslls.F90
+++ src/slls/useslls.F90
@@ -389,7 +389,7 @@
           END IF
           DO i = ir, ic
             state = ' FREE'
-            IF ( ABS( prob%X( i ) < ten * stopr ) ) state = 'LOWER'
+            IF ( ABS( prob%X( i ) ) < ten * stopr ) state = 'LOWER'
             WRITE( out, 2050 ) i, VNAME( i ), state, prob%X( i ),              &
                                prob%Z( i )
           END DO
