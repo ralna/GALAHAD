3ddb3067303537a7b7193b98f27ee6953178c812
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-02-14 01:16:08 -0500

    Add modules.yml

---
+++ .github/julia/check_modules.jl
@@ -0,0 +1,37 @@
+using Test
+
+function exported_modules(path::String)
+  modules = String[]
+  files = readdir(path)
+  for file in files
+    if endswith(file, ".mod")
+      push!(modules, file)
+    end
+  end
+  return modules
+end
+
+modules_single_int32 = exported_modules(joinpath(@__DIR__, "..", "..", "builddir_int32", "libgalahad_single.so.p"))
+modules_double_int32 = exported_modules(joinpath(@__DIR__, "..", "..", "builddir_int32", "libgalahad_double.so.p"))
+modules_single_int64 = exported_modules(joinpath(@__DIR__, "..", "..", "builddir_int64", "libgalahad_single.so.p"))
+modules_double_int64 = exported_modules(joinpath(@__DIR__, "..", "..", "builddir_int64", "libgalahad_double.so.p"))
+
+modules_combinations = [(modules_single_int32, modules_double_int32, 32, 32, "libgalahad_single.so (Int32) and libgalahad_double.so (Int32)"),
+                        (modules_single_int32, modules_single_int64, 32, 64, "libgalahad_single.so (Int32) and libgalahad_single.so (Int64)"),
+                        (modules_single_int32, modules_double_int64, 32, 64, "libgalahad_single.so (Int32) and libgalahad_double.so (Int64)"),
+                        (modules_double_int32, modules_single_int64, 32, 64, "libgalahad_double.so (Int32) and libgalahad_single.so (Int64)"),
+                        (modules_double_int32, modules_double_int64, 32, 64, "libgalahad_double.so (Int32) and libgalahad_double.so (Int64)"),
+                        (modules_double_int64, modules_double_int64, 64, 64, "libgalahad_double.so (Int64) and libgalahad_double.so (Int64)")]
+
+single_double_modules = String[]
+
+for (modules1, modules2, int1, int2, name) in modules_combinations
+  intersect_modules = intersect(modules1, modules2)
+  println("---------------------------------------------------------------------------------------------------------------------------")
+  @warn("The following modules are generated for both libraries $name.")
+  for mod in intersect_modules
+    println(symbol)
+  end
+  println("---------------------------------------------------------------------------------------------------------------------------")
+  println()
+end
