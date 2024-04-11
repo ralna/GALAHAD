a9fb9fea1f564b74d58dc30a7abbd7a8417b8519
Nick Gould <dalekopera@gmail.com>
2024-04-11 15:20:43 +0100

    Added missing adapter and metis

---
+++ src/metis/adapter.c
@@ -0,0 +1,83 @@
+/* Copyright (C) 2020 COIN-OR
+ * All Rights Reserved.
+ * This file is distributed under the Eclipse Public License.
+ */
+
+#ifdef INTEGER_64
+#define galahad_metis5_adapter galahad_metis5_adapter_64
+#define METIS_NodeND METIS_NodeND_64
+#define METIS_SetDefaultOptions METIS_SetDefaultOptions_64
+#endif
+
+#include "metis.h" /* from MeTiS 5 */
+#include "stdio.h"
+
+/* name change to avoid any possible conflicts */
+
+void galahad_metis5_adapter(idx_t* nvtxs, idx_t* xadj, idx_t* adjncy, 
+                            idx_t* numflag, idx_t* options, idx_t* perm, 
+                            idx_t* iperm){
+    idx_t options5[METIS_NOPTIONS];
+
+    /* Handle MA57 pathological case */
+    if(*nvtxs == 1){
+        /* MA57 seems to call metis with a graph containing 1 vertex and
+         * a self-loop. Metis5 prints an error for this.
+         */
+        perm[0] = *numflag;
+        iperm[0] = *numflag;
+        return;
+    }
+
+    /* Set default MeTiS 5 options */
+    METIS_SetDefaultOptions(options5);
+    options5[METIS_OPTION_NUMBERING] = *numflag;
+    // options5[METIS_OPTION_OBJTYPE] = METIS_OBJTYPE_NODE;
+
+    /* Translate MeTiS 4 options MeTiS 5 options */
+    if(options[0] != 0){
+        if(options[1] == 1) /* random matching */
+            options5[METIS_OPTION_CTYPE] = METIS_CTYPE_RM;
+        else /* heavy-edge or sorted heavy-edge matching; map both to shem,
+                as heave-edge matching not available in metis5 */
+            options5[METIS_OPTION_CTYPE] = METIS_CTYPE_SHEM;
+
+        if(options[2] == 1) /* edge-based region-growing */
+            options5[METIS_OPTION_IPTYPE] = METIS_IPTYPE_EDGE;
+        else /* node-based region-growing */
+            options5[METIS_OPTION_IPTYPE] = METIS_IPTYPE_NODE;
+
+        if(options[3] == 1) /* two-sided node FM refinement */
+            options5[METIS_OPTION_RTYPE] = METIS_RTYPE_SEP2SIDED;
+        else  /* one-sided node FM refinement */
+            options5[METIS_OPTION_RTYPE] = METIS_RTYPE_SEP1SIDED;
+
+        options5[METIS_OPTION_DBGLVL] = options[4];
+
+        switch(options[5]){
+            case 0:  /* do not try to compress or order connected components */
+                options5[METIS_OPTION_COMPRESS] = 0;
+                options5[METIS_OPTION_CCORDER] = 0;
+                break;
+            case 1:  /* try to compress graph */
+                options5[METIS_OPTION_COMPRESS] = 1;
+                options5[METIS_OPTION_CCORDER] = 0;
+                break;
+            case 2:  /* order each component separately */
+                options5[METIS_OPTION_COMPRESS] = 0;
+                options5[METIS_OPTION_CCORDER] = 1;
+                break;
+            case 3:  /* try to compress and order components */
+                options5[METIS_OPTION_COMPRESS] = 1;
+                options5[METIS_OPTION_CCORDER] = 1;
+                break;
+        }
+
+        options5[METIS_OPTION_PFACTOR] = options[6];
+
+        options5[METIS_OPTION_NSEPS] = options[7];
+    }
+
+    /* Call MeTiS 5 to get ordering */
+    METIS_NodeND(nvtxs, xadj, adjncy, (void*)0, options5, perm, iperm);
+}
---
+++ src/metis/metis.f90
@@ -0,0 +1,70 @@
+! THIS VERSION: GALAHAD 5.0 - 2024-03-25 AT 08:25 GMT.
+
+#ifdef INTEGER_64
+#define galahad_kinds galahad_kinds_64
+#define galahad_metis galahad_metis_64
+#define galahad_metis_setup galahad_metis_setup_64
+#define galahad_metis5_adapter galahad_metis5_adapter_64
+#endif
+
+!  MeTiS 4 and 5 interfaces
+
+  SUBROUTINE galahad_metis( n, iptr, irn, metftn, metopt, invprm, perm )
+    USE galahad_kinds, ONLY: ip_
+    IMPLICIT NONE
+    INTEGER( KIND = ip_ ), iNTENT( IN ) :: n
+    INTEGER( KIND = ip_ ), INTENT( IN ), DIMENSION( n + 1 ) :: iptr
+    INTEGER( KIND = ip_ ), INTENT( IN ), DIMENSION( * ) :: irn
+    INTEGER( KIND = ip_ ), INTENT( IN ) :: metftn
+    INTEGER( KIND = ip_ ), INTENT( IN ), DIMENSION( 8 ) :: metopt
+    INTEGER( KIND = ip_ ), INTENT( OUT ), DIMENSION( n ) :: invprm
+    INTEGER( KIND = ip_ ), INTENT( OUT ), DIMENSION( n ) :: perm
+
+#ifdef METIS4
+    ! Call MeTiS 4 to get ordering
+    CALL metis_nodend( n, iptr, irn, metftn, metopt, invprm, perm )
+#else
+    ! C interface for MeTiS 4 to 5 adapter
+    INTERFACE
+      SUBROUTINE galahad_metis5_adapter( nvtxs, xadj, adjncy, numflag,         &
+                                         options, perm, iperm ) BIND(C)
+        USE galahad_kinds, ONLY: ipc_
+        IMPLICIT NONE
+        INTEGER( KIND = ipc_ ), INTENT( IN ) :: nvtxs, numflag
+        INTEGER( KIND = ipc_ ), DIMENSION( * ), INTENT( IN ) :: xadj, adjncy
+        INTEGER( KIND = ipc_ ), DIMENSION( * ), INTENT( IN ) :: options
+        INTEGER( KIND = ipc_ ), DIMENSION( * ), INTENT( OUT ) :: perm, iperm
+      END SUBROUTINE galahad_metis5_adapter
+    END INTERFACE
+    ! call MeTiS 5 to get ordering via C MeTiS 4 to 5 adapter
+    CALL galahad_metis5_adapter( n, iptr, irn, metftn, metopt, invprm, perm )
+#endif
+  RETURN
+  END SUBROUTINE galahad_metis
+
+!  MeTiS 4 and 5 default options
+
+  SUBROUTINE galahad_metis_setopt( metopt )
+    USE galahad_kinds, ONLY: ip_
+    INTEGER( KIND = ip_ ), INTENT( OUT ), DIMENSION( 8 ) :: metopt
+#ifdef METIS4
+    metopt = (/ 0,  & ! OPTION_PTYPE
+                3,  & ! OPTION_CTYPE
+                1,  & ! OPTION_ITYPE
+                1,  & ! OPTION_RTYPE
+                0,  & ! OPTION_DBGLVL (not default)
+                1,  & ! OPTION_OFLAGS
+                -1, & ! OPTION_PFACTOR
+                1  /) ! OPTION_NSEPS
+#else
+    metopt = (/ 1, & ! set options
+                0, & ! METIS_OPTION_CTYPE
+                1, & ! METIS_OPTION_IPTYPE
+                0, & ! METIS_OPTION_RTYPE
+                0, & ! METIS_OPTION_DBGLVL (not default)
+                1, & ! METIS_OPTION_COMPRESS and _CCORDER
+                0, & ! METIS_OPTION_PFACTOR
+                1 /) ! METIS_OPTION_NSEPS
+#endif
+  RETURN
+  END SUBROUTINE galahad_metis_setopt
