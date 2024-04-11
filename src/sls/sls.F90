477755826aebd51402625593f8cbd550582eca0d
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-04-11 10:22:41 -0400

    Add missing dummy HSL routines

--- src/sls/sls.F90
+++ src/sls/sls.F90
@@ -1001,8 +1001,8 @@
 
 !  check to see if the MeTiS ordering packages is available
 
-     CALL galahad_metis_setopt( ICNTL_metis )
-     CALL galahad_metis( n_dummy, PTR, ROW, 1, ICNTL_metis, INVP, PERM )
+     CALL hsl_metis_setopt( ICNTL_metis )
+     CALL hsl_metis( n_dummy, PTR, ROW, 1, ICNTL_metis, INVP, PERM )
      metis_available = PERM( 1 ) > 0
 !write(6,*) ' hsl_available, metis_available ', hsl_available, metis_available
 !write(6,*) ' solver ', TRIM( inform%solver )
