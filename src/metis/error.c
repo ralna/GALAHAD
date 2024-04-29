8c1be60805baef46e25b7adc063bbce9f149709f
nimgould <nick.gould@stfc.ac.uk>
2024-04-29 14:02:21 +0100

    remove attempt to use strerror_r in error.c as it is unreliable

--- src/metis/error.c
+++ src/metis/error.c
@@ -37,7 +37,7 @@ static int gk_exit_on_error = 1;
 
 
 /*************************************************************************/
-/*! This function sets the gk_exit_on_error variable 
+/*! This function sets the gk_exit_on_error variable
  */
 /*************************************************************************/
 void gk_set_exit_on_error(int value)
@@ -48,7 +48,7 @@ void gk_set_exit_on_error(int value)
 
 
 /*************************************************************************/
-/*! This function prints an error message and exits  
+/*! This function prints an error message and exits
  */
 /*************************************************************************/
 void errexit(char *f_str,...)
@@ -91,11 +91,11 @@ void gk_errexit(int signum, char *f_str,...)
 
 
 /***************************************************************************/
-/*! This function sets a number of signal handlers and sets the return point 
+/*! This function sets a number of signal handlers and sets the return point
     of a longjmp
 */
 /***************************************************************************/
-int gk_sigtrap() 
+int gk_sigtrap()
 {
   if (gk_cur_jbufs+1 >= MAX_JBUFS)
     return 0;
@@ -107,13 +107,13 @@ int gk_sigtrap()
 
   return 1;
 }
-  
+
 
 /***************************************************************************/
 /*! This function sets the handlers for the signals to their default handlers
  */
 /***************************************************************************/
-int gk_siguntrap() 
+int gk_siguntrap()
 {
   if (gk_cur_jbufs == -1)
     return 0;
@@ -125,69 +125,71 @@ int gk_siguntrap()
 
   return 1;
 }
-  
+
 
 /*************************************************************************/
 /*! This function is the custome signal handler, which all it does is to
-    perform a longjump to the most recent saved environment 
+    perform a longjump to the most recent saved environment
  */
 /*************************************************************************/
 void gk_sigthrow(int signum)
 {
   longjmp(gk_jbufs[gk_cur_jbufs], signum);
 }
-  
+
 
 /***************************************************************************
-* This function sets a number of signal handlers and sets the return point 
+* This function sets a number of signal handlers and sets the return point
 * of a longjmp
 ****************************************************************************/
-void gk_SetSignalHandlers() 
+void gk_SetSignalHandlers()
 {
   old_SIGMEM_handler = signal(SIGMEM,  gk_NonLocalExit_Handler);
   old_SIGERR_handler = signal(SIGERR,  gk_NonLocalExit_Handler);
 }
-  
+
 
 /***************************************************************************
 * This function sets the handlers for the signals to their default handlers
 ****************************************************************************/
-void gk_UnsetSignalHandlers() 
+void gk_UnsetSignalHandlers()
 {
   signal(SIGMEM,  old_SIGMEM_handler);
   signal(SIGERR,  old_SIGERR_handler);
 }
-  
+
 
 /*************************************************************************
-* This function is the handler for SIGUSR1 that implements the cleaning up 
+* This function is the handler for SIGUSR1 that implements the cleaning up
 * process prior to a non-local exit.
 **************************************************************************/
 void gk_NonLocalExit_Handler(int signum)
 {
   longjmp(gk_jbuf, signum);
 }
-  
+
 
 /*************************************************************************/
 /*! \brief Thread-safe implementation of strerror() */
 /**************************************************************************/
 char *gk_strerror(int errnum)
+/* comment this out as it is more trouble than it is worth! */
+/* { */
+/* #if defined(WIN32) || defined(__MINGW32__) */
+/*   return strerror(errnum); */
+/* #else  */
+/* #ifndef SUNOS */
+/*   static __thread char buf[1024]; */
+/*   strerror_r(errnum, buf, 1024); */
+/*   buf[1023] = '\0'; */
+/*   return buf; */
+/* #else */
+/*   return strerror(errnum); */
+/* #endif */
+/* #endif */
+/* } */
 {
-#if defined(WIN32) || defined(__MINGW32__)
   return strerror(errnum);
-#else 
-#ifndef SUNOS
-  static __thread char buf[1024];
-
-  strerror_r(errnum, buf, 1024);
-
-  buf[1023] = '\0';
-  return buf;
-#else
-  return strerror(errnum);
-#endif
-#endif
 }
 
 
@@ -204,7 +206,7 @@ void PrintBackTrace()
 
   size = backtrace(array, 10);
   strings = backtrace_symbols(array, size);
-  
+
   printf("Obtained %d stack frames.\n", size);
   for (i=0; i<size; i++) {
     printf("%s\n", strings[i]);
