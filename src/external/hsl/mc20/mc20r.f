12814b9d3dfda801cd5d7efceb82b878b47ac3f7
nimgould <nick.gould@stfc.ac.uk>
2024-04-29 11:55:49 +0100

    full interface to dummy hsl mc20 provided

--- src/external/hsl/mc20/mc20r.f
+++ src/external/hsl/mc20/mc20r.f
@@ -1,7 +1,24 @@
-C  THIS VERSION: GALAHAD 5.0 - 2024-03-17 AT 09:00 GMT.
+C  THIS VERSION: GALAHAD 5.0 - 2024-04-29 AT 12:00 GMT.
 
 #include "hsl_subset.h"
 
-      SUBROUTINE MC20AR( )
-      END SUBROUTINE MC20AR
+      SUBROUTINE MC20AR( nc, maxa, A, INUM, JPTR, JNUM, jdisp )
+      USE HSL_KINDS_real, ONLY: ip_, rp_
 
+!-----------------------------------------------
+!   D u m m y   A r g u m e n t s
+!-----------------------------------------------
+
+      INTEGER(ip_), INTENT( IN ) :: nc, maxa, jdisp
+      INTEGER(ip_), INTENT( INOUT ), DIMENSION( maxa ) :: INUM, JNUM
+      INTEGER(ip_), INTENT( OUT ), DIMENSION( nc ) :: JPTR
+      REAL(rp_), INTENT( INOUT ), DIMENSION( maxa ) :: A
+
+!  Dummy subroutine available with GALAHAD
+
+      JPTR( 1 ) = - 1_ip_
+      RETURN
+
+!  End of dummy subroutine MC20AD
+
+      END SUBROUTINE MC20AR
