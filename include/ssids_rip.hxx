34b54a624bcd1681aaf6dcc4eb29ae52dbdd71e4
nimgould <nick.gould@stfc.ac.uk>
2024-06-20 08:05:39 +0100

    update C header file for ssids to be consistent with hsl_recision

--- include/ssids_rip.hxx
+++ include/ssids_rip.hxx
@@ -2,10 +2,14 @@
  *  \copyright 2024 GALAHAD productions
  *  \licence   BSD licence, see LICENCE file for details
  *  \author    Nick Gould
- *  \version   GALAHAD 5.0 - 2024-06-11 AT 08:30 GMT
+ *  \version   GALAHAD 5.0 - 2024-06-20 AT 07:50 GMT
  */
 
 #include <stdint.h>
+
+/* include guard */
+#ifndef SSIDS_RIP_H
+#define SSIDS_RIP_H
 
 /* real precision employed */
 
@@ -25,6 +29,17 @@ typedef int ipc_;
 typedef unsigned int uipc_;
 #endif
 
-/* generic storage */
+/* C long integer */
 
+#ifdef INTEGER_64
 typedef int64_t longc_;
+#else
+#ifdef HSL_LEGACY
+typedef long longc_;
+#else
+typedef int64_t longc_;
+#endif
+#endif
+
+/* end include guard */
+#endif
