8fc4a2374a91abcf8ea4431e4696f7eeea57f9ea
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-08-16 15:39:21 -0400

    Update galahad.h

--- include/galahad_version.h
+++ include/galahad_version.h
@@ -1,2 +1,9 @@
+// include guard
+#ifndef GALAHAD_VERSION_H
+#define GALAHAD_VERSION_H
+
 #include "galahad_precision.h"
 void version_galahad(ipc_ *major, ipc_ *minor, ipc_ *patch);
+
+// end include guard
+#endif
