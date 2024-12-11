61ecaa6cde942cdbdd630c52906d483455806f41
Nick Gould <dalekopera@gmail.com>
2024-12-11 08:15:18 +0000

    added missing galahad_quadruple.h and use use galahad_kind to galahad_error.F90

---
+++ include/galahad_quadruple.h
@@ -0,0 +1,24 @@
+/*
+ * THIS VERSION: GALAHAD 5.1 - 2024-11-21 AT 08:45 GMT.
+ *
+ *-*-*-*-*-*-*-*-*-  GALAHAD C INTERFACE PRECISION  *-*-*-*-*-*-*-*-*-*-
+ *
+ *  Copyright reserved, Gould/Orban/Toint, for GALAHAD productions
+ *  Principal author: Jaroslav Fowkes
+ *
+ *  History -
+ *   originally released GALAHAD Version 3.3. July 27th 2021
+ *
+ *  For full documentation, see
+ *   http://galahad.rl.ac.uk/galahad-www/specs.html
+ */
+
+// include guard
+#ifndef GALAHAD_PRECISION_H
+#define GALAHAD_PRECISION_H
+
+typedef __float128 rpc_;  // working precision
+typedef float real_sp_;   // single precision
+
+// end include guard
+#endif
