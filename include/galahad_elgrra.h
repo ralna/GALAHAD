ef4048e56c929853765a2f9f9c5e6ab3cd04f22e
Nick Gould <dalekopera@gmail.com>
2024-12-13 07:35:19 +0000

    added REAL_128 clause to elgrra header file

--- include/galahad_elgrra.h
+++ include/galahad_elgrra.h
@@ -6,7 +6,7 @@
  *
  * Nick Gould for GALAHAD
  * initial version, 2023-11-15
- * this version 2024-06-11
+ * this version 2024-12-13
  */
 
 #ifdef REAL_32
@@ -14,6 +14,11 @@
 #define ELFUN_flexible_r ELFUN_flexible_s
 #define GROUP_r GROUP_s
 #define RANGE_r RANGE_s
+#elif REAL_128
+#define ELFUN_r ELFUN_q
+#define ELFUN_flexible_r ELFUN_flexible_q
+#define GROUP_r GROUP_q
+#define RANGE_r RANGE_q
 #else
 #define ELFUN_r ELFUN
 #define ELFUN_flexible_r ELFUN_flexible
