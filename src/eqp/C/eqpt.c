5093b6e3a90dba0f758719413d1150aa7260e16c
Nick Gould <dalekopera@gmail.com>
2024-12-17 15:11:36 +0000

    test+++

--- src/eqp/C/eqpt.c
+++ src/eqp/C/eqpt.c
@@ -140,9 +140,9 @@ int main(void) {
 // interim replacement for quad output: $GALAHAD/include/galahad_pquad_if.h
             printf(" before replace \n");
 //#include "galahad_pquad_if.h"
-//            printf("%c:%6" i_ipc_ " cg iterations. Optimal objective " 
-//                   "status = %1" i_ipc_ "\n",
-//                   st, inform.cg_iter, inform.status);
+            printf("%c:%6" i_ipc_ " cg iterations. Optimal objective " 
+                   "value = %.2Qf status = %1" i_ipc_ "\n",
+                   st, inform.cg_iter, inform.obj, inform.status);
             printf(" after replace \n");
 #else
             printf("%c:%6" i_ipc_ " cg iterations. Optimal objective " 
