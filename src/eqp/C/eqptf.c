8d45f3fa7ad3cedeeee98af06988b88d2508e729
Nick Gould <dalekopera@gmail.com>
2024-12-18 09:15:54 +0000

    remove quadmath calls for eqp and rpd C tests

--- src/eqp/C/eqptf.c
+++ src/eqp/C/eqptf.c
@@ -126,13 +126,7 @@ int main(void) {
 
         if(inform.status == 0){
 #ifdef REAL_128
-            char buff[128];
-            int nf = quadmath_snprintf(buff, sizeof buff,
-                                       "%*.2Qf", inform.obj);
-            if ((size_t) nf < sizeof buff) 
-              printf("%c:%6" i_ipc_ " cg iterations. Optimal objective " 
-                     "value = %s status = %1" i_ipc_ "\n",
-                     st, inform.cg_iter, buff, inform.status);
+#include "galahad_pquad_if.h"
 #else
             printf("%c:%6" i_ipc_ " cg iterations. Optimal objective " 
                    "value = %.2f status = %1" i_ipc_ "\n",
@@ -190,13 +184,7 @@ int main(void) {
 
         if(inform.status == 0){
 #ifdef REAL_128
-            char buff[128];
-            int nf = quadmath_snprintf(buff, sizeof buff,
-                                       "%*.2Qf", inform.obj);
-            if ((size_t) nf < sizeof buff) 
-              printf("%c:%6" i_ipc_ " cg iterations. Optimal objective " 
-                     "value = %s status = %1" i_ipc_ "\n",
-                     st, inform.cg_iter, buff, inform.status);
+#include "galahad_pquad_if.h"
 #else
             printf("%c:%6" i_ipc_ " cg iterations. Optimal objective " 
                    "value = %.2f status = %1" i_ipc_ "\n",
