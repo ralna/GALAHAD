6fa6db9434608102a624fffd36c23acd872a9a3e
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-01-04 21:01:37 +0100

    Update again test_sbls.jl

--- GALAHAD.jl/test/test_sbls.jl
+++ GALAHAD.jl/test/test_sbls.jl
@@ -55,10 +55,8 @@ function test_sbls(::Type{T}, ::Type{INT}) where {T,INT}
     @reset control[].preconditioner = INT(2)
     @reset control[].factorization = INT(2)
     @reset control[].get_norm_residual = true
-
-    # debug
-    @reset control[].print_level = INT(1)
-    @reset control[].sls_control.print_level = INT(1)
+    @reset control[].symmetric_linear_solver = galahad_linear_solver("sytr")
+    @reset control[].definite_linear_solver = galahad_linear_solver("sytr")
 
     # Set user-defined control options
     @reset control[].f_indexing = true # fortran sparse matrix indexing
