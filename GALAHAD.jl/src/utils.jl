a0207adcd80e770c6d6cdd8149d2257d17014259
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-01-24 10:00:19 -0600

    On linux ARM, a Cchar is a UInt8 and not a Int8

--- GALAHAD.jl/src/utils.jl
+++ GALAHAD.jl/src/utils.jl
@@ -4,7 +4,7 @@ function galahad_linear_solver(solver::String)
   nchar = length(solver)
   (nchar > 30) && error("The solver can't have more than 30 characters.")
 
-  result = zeros(Int8, 31)
+  result = zeros(Cchar, 31)
   for i in 1:nchar
     result[i] = Cchar(solver[i])
   end
