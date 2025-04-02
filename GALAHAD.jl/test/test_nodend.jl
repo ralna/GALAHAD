7b1079ddef6d80fbc7e07c0754bec70aa0be556b
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-04-01 23:58:57 -0500

    Fix again the Julia tests for nodend

--- GALAHAD.jl/test/test_nodend.jl
+++ GALAHAD.jl/test/test_nodend.jl
@@ -49,7 +49,7 @@ function test_nodend(::Type{T}, ::Type{INT}) where {T,INT}
     nodend_initialize(T, INT, data, control, status)
 
     # Set user-defined control options
-    control[].f_indexing = true  # Fortran sparse matrix indexing
+    @reset control[].f_indexing = true  # Fortran sparse matrix indexing
 
     # sparse co-ordinate storage
     if d == 1
