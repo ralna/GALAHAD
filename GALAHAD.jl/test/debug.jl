fc2ab3fab01edeef81dd34a6563a6599c8409eda
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-04-07 00:54:51 -0500

    Why do we have a different error if we use f_indexing before ccqp_initialize

--- GALAHAD.jl/test/debug.jl
+++ GALAHAD.jl/test/debug.jl
@@ -45,11 +45,11 @@ status = Ref{INT}()
 @printf(" Fortran sparse matrix indexing\n\n")
 @printf(" basic tests of qp storage formats\n\n")
 
-# Initialize CCQP
-ccqp_initialize(T, INT, data, control, status)
-
 # Set user-defined control options
 global control = @reset control[].f_indexing = true # Fortran sparse matrix indexing
+
+# Initialize CCQP
+ccqp_initialize(T, INT, data, control, status)
 
 # Start from 0
 x = T[0.0, 0.0, 0.0]
