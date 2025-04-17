3618cc410da134b9069e3e6f191c96a245ff45e5
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-04-17 17:27:24 -0500

    Only use GALAHAD symbols in runtrb_sif.F90

--- src/cutest_functions/cutest_trampoline.F90
+++ src/cutest_functions/cutest_trampoline.F90
@@ -1,28 +1,26 @@
 ! THIS VERSION: GALAHAD 5.2 - 2025-04-17 AT 16:20 GMT.
 
+#include "galahad_modules.h"
+
 #ifdef REAL_32
-#define GALAHAD_LOAD_ROUTINES_NAME galahad_load_routines_s
-#define GALAHAD_UNLOAD_ROUTINES_NAME galahad_unload_routines_s
-#define CUTEST_LOAD_ROUTINES_NAME cutest_load_routines_s
-#define CUTEST_UNLOAD_ROUTINES_NAME cutest_unload_routines_s
+#define galahad_load_routines_r galahad_load_routines_s
+#define galahad_unload_routines_r galahad_unload_routines_s
 #elif REAL_128
-#define GALAHAD_LOAD_ROUTINES_NAME galahad_load_routines_q
-#define GALAHAD_UNLOAD_ROUTINES_NAME galahad_unload_routines_q
-#define CUTEST_LOAD_ROUTINES_NAME cutest_load_routines_q
-#define CUTEST_UNLOAD_ROUTINES_NAME cutest_unload_routines_q
+#define galahad_load_routines_r galahad_load_routines_q
+#define galahad_unload_routines_r galahad_unload_routines_q
 #else
-#define GALAHAD_LOAD_ROUTINES_NAME galahad_load_routines
-#define GALAHAD_UNLOAD_ROUTINES_NAME galahad_unload_routines
-#define CUTEST_LOAD_ROUTINES_NAME cutest_load_routines
-#define CUTEST_UNLOAD_ROUTINES_NAME cutest_unload_routines
+#define galahad_load_routines_r galahad_load_routines
+#define galahad_unload_routines_r galahad_unload_routines
 #endif
 
-SUBROUTINE GALAHAD_LOAD_ROUTINES_NAME(libname)
+SUBROUTINE galahad_load_routines_r(libname)
+    USE CUTEST_TRAMPOLINE_precision
     USE ISO_C_BINDING, ONLY : C_CHAR
     CHARACTER ( KIND = C_CHAR ), DIMENSION( * ), INTENT( IN ) :: libname
-    CALL CUTEST_LOAD_ROUTINES_NAME(libname)
-END SUBROUTINE GALAHAD_LOAD_ROUTINES_NAME
+    CALL CUTEST_LOAD_ROUTINES(libname)
+END SUBROUTINE galahad_load_routines_r
 
-SUBROUTINE GALAHAD_UNLOAD_ROUTINES_NAME()
-    CALL CUTEST_UNLOAD_ROUTINES_NAME()
-END SUBROUTINE GALAHAD_UNLOAD_ROUTINES_NAME
+SUBROUTINE galahad_unload_routines_r()
+    USE CUTEST_TRAMPOLINE_precision
+    CALL CUTEST_UNLOAD_ROUTINES()
+END SUBROUTINE galahad_unload_routines_r
