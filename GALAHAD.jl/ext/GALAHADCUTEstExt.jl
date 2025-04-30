f65487cb587cb31701adb723351157dc56fd6f3b
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-04-30 00:04:33 -0500

    Use CUTEst.jl v1.2.1 for run_sif

--- GALAHAD.jl/ext/GALAHADCUTEstExt.jl
+++ GALAHAD.jl/ext/GALAHADCUTEstExt.jl
@@ -2,20 +2,23 @@ module GALAHADCUTEstExt
 
 import GALAHAD
 import CUTEst
-using Libdl
 
-function GALAHAD.run_sif(solver::Symbol, precision::Symbol, sif::String, args...; decode::Bool=true, verbose::Bool=false)
-	if decode
-		CUTEst.sifdecoder(sif, args...; verbose, precision)
-		CUTEst.build_libsif(sif; precision)
-	end
-	path_libsif = joinpath(CUTEst.libsif_path, "lib$(sif)_$(precision).$dlext")
-	path_outsdif = joinpath(CUTEst.libsif_path, CUTEst._name_outsdif(sif, precision))
-	if isfile(path_libsif) && isfile(path_outsdif)
-		GALAHAD.run_sif(Val(solver), Val(precision), path_libsif, path_outsdif)
-	else
-		error("The SIF problem $sif was not decoded.")
-	end
+function GALAHAD.run_sif(solver::Symbol, precision::Symbol, sif::String, args...; decode::Bool=true,
+                         verbose::Bool=false, libsif_folder::String=CUTEst.libsif_path)
+  standalone = true
+  if decode
+    CUTEst.sifdecoder(sif, args...; verbose, precision, libsif_folder)
+    CUTEst.build_libsif(sif; precision, standalone, libsif_folder)
+  end
+  name_libsif = CUTEst._name_libsif(sif, precision; standalone)
+  path_libsif = joinpath(libsif_folder, name_libsif)
+  name_outsdif = CUTEst._name_outsdif(sif, precision)
+  path_outsdif = joinpath(libsif_folder, name_outsdif)
+  if isfile(path_libsif) && isfile(path_outsdif)
+    GALAHAD.run_sif(Val(solver), Val(precision), path_libsif, path_outsdif)
+  else
+    error("The SIF problem $sif was not decoded.")
+  end
 end
 
 end  # module GALAHADCUTEstExt
