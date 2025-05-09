9f9d7d522c015540f2ffe813142d1ec8ad69c3b8
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2025-05-09 12:08:31 -0500

    Update build_tarballs.jl

--- .github/julia/build_tarballs.jl
+++ .github/julia/build_tarballs.jl
@@ -103,7 +103,8 @@ dependencies = [
     Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2")),
     Dependency(PackageSpec(name="OpenBLAS_jll", uuid="4536629a-c528-5b80-bd46-f80d51c5b363")),
     Dependency(PackageSpec(name="Hwloc_jll", uuid="e33a78d0-f292-5ffc-b300-72abe9b543c8")),
-    Dependency(PackageSpec(name="MUMPS_seq_jll", uuid="d7ed1dd3-d0ae-5e8e-bfb4-87a502085b8d"), compat="=5.4.1"),
+    # Dependency(PackageSpec(name="MUMPS_seq_jll", uuid="d7ed1dd3-d0ae-5e8e-bfb4-87a502085b8d"), compat="=5.4.1"),
+    Dependency(PackageSpec(url="https://github.com/amontoison/MUMPS_seq_jll.jl", rev="main")),
     Dependency(PackageSpec(name="HSL_jll", uuid="017b0a0e-03f4-516a-9b91-836bbd1904dd")),
     Dependency(PackageSpec(name="CUTEst_jll", uuid="1b53aba6-35b6-5f92-a507-53c67d53f819"), compat="2.5.1"),
 ]
