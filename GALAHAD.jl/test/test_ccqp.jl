8585b36a64a58bea99296d3e2d28ec7a30ab593a
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-07-17 20:36:23 -0500

    Update test_ccqp.jl

--- GALAHAD.jl/test/test_ccqp.jl
+++ GALAHAD.jl/test/test_ccqp.jl
@@ -234,7 +234,7 @@ for (T, INT, libgalahad) in ((Float32 , Int32, GALAHAD.libgalahad_single      ),
                              (Float64 , Int64, GALAHAD.libgalahad_double_64   ),
                              (Float128, Int32, GALAHAD.libgalahad_quadruple   ),
                              (Float128, Int64, GALAHAD.libgalahad_quadruple_64))
-  if isfile(libgalahad)
+  if isfile(libgalahad) && !Sys.isapple()
     @testset "CCQP -- $T -- $INT" begin
       @test test_ccqp(T, INT) == 0
     end
