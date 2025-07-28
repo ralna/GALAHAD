8c7eb7ca2de1e5edc5c69f8f9e0cb50efa3ca6e4
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-07-28 17:44:28 -0500

    [GALAHAD.jl] Add tests for the package VERSION

--- GALAHAD.jl/src/wrappers/version.jl
+++ GALAHAD.jl/src/wrappers/version.jl
@@ -1,6 +1,14 @@
 export version_galahad
 
-function version_galahad()
+function version_galahad(::Type{Float32}, ::Type{Int32})
+  major = Ref{Int32}(0)
+  minor = Ref{Int32}(0)
+  patch = Ref{Int32}(0)
+  @ccall libgalahad_single.version_galahad(major::Ref{Int32}, minor::Ref{Int32}, patch::Ref{Int32})::Cvoid
+  return VersionNumber(major[], minor[], patch[])
+end
+
+function version_galahad(::Type{Float64}, ::Type{Int32})
   major = Ref{Int32}(0)
   minor = Ref{Int32}(0)
   patch = Ref{Int32}(0)
@@ -8,12 +16,34 @@ function version_galahad()
   return VersionNumber(major[], minor[], patch[])
 end
 
-export version_galahad_64
+function version_galahad(::Type{Float128}, ::Type{Int32})
+  major = Ref{Int32}(0)
+  minor = Ref{Int32}(0)
+  patch = Ref{Int32}(0)
+  @ccall libgalahad_quadruple.version_galahad(major::Ref{Int32}, minor::Ref{Int32}, patch::Ref{Int32})::Cvoid
+  return VersionNumber(major[], minor[], patch[])
+end
 
-function version_galahad_64()
+function version_galahad(::Type{Float32}, ::Type{Int64})
   major = Ref{Int64}(0)
   minor = Ref{Int64}(0)
   patch = Ref{Int64}(0)
-  @ccall libgalahad_double_64.version_galahad_64(major::Ref{Int64}, minor::Ref{Int64}, patch::Ref{Int64})::Cvoid
+  @ccall libgalahad_single_64.version_galahad(major::Ref{Int64}, minor::Ref{Int64}, patch::Ref{Int64})::Cvoid
+  return VersionNumber(major[], minor[], patch[])
+end
+
+function version_galahad(::Type{Float64}, ::Type{Int64})
+  major = Ref{Int64}(0)
+  minor = Ref{Int64}(0)
+  patch = Ref{Int64}(0)
+  @ccall libgalahad_double_64.version_galahad(major::Ref{Int64}, minor::Ref{Int64}, patch::Ref{Int64})::Cvoid
+  return VersionNumber(major[], minor[], patch[])
+end
+
+function version_galahad(::Type{Float128}, ::Type{Int64})
+  major = Ref{Int64}(0)
+  minor = Ref{Int64}(0)
+  patch = Ref{Int64}(0)
+  @ccall libgalahad_quadruple_64.version_galahad(major::Ref{Int64}, minor::Ref{Int64}, patch::Ref{Int64})::Cvoid
   return VersionNumber(major[], minor[], patch[])
 end
