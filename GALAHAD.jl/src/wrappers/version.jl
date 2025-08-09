2d05fd0c13ff98c92f3f3de19ee93bdc25a67818
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-08-09 15:55:34 -0500

    Fix version.jl

--- GALAHAD.jl/src/wrappers/version.jl
+++ GALAHAD.jl/src/wrappers/version.jl
@@ -28,7 +28,7 @@ function version_galahad(::Type{Float32}, ::Type{Int64})
   major = Ref{Int64}(0)
   minor = Ref{Int64}(0)
   patch = Ref{Int64}(0)
-  @ccall libgalahad_single_64.version_galahad(major::Ref{Int64}, minor::Ref{Int64}, patch::Ref{Int64})::Cvoid
+  @ccall libgalahad_single_64.version_galahad_64(major::Ref{Int64}, minor::Ref{Int64}, patch::Ref{Int64})::Cvoid
   return VersionNumber(major[], minor[], patch[])
 end
 
@@ -36,7 +36,7 @@ function version_galahad(::Type{Float64}, ::Type{Int64})
   major = Ref{Int64}(0)
   minor = Ref{Int64}(0)
   patch = Ref{Int64}(0)
-  @ccall libgalahad_double_64.version_galahad(major::Ref{Int64}, minor::Ref{Int64}, patch::Ref{Int64})::Cvoid
+  @ccall libgalahad_double_64.version_galahad_64(major::Ref{Int64}, minor::Ref{Int64}, patch::Ref{Int64})::Cvoid
   return VersionNumber(major[], minor[], patch[])
 end
 
@@ -44,6 +44,6 @@ function version_galahad(::Type{Float128}, ::Type{Int64})
   major = Ref{Int64}(0)
   minor = Ref{Int64}(0)
   patch = Ref{Int64}(0)
-  @ccall libgalahad_quadruple_64.version_galahad(major::Ref{Int64}, minor::Ref{Int64}, patch::Ref{Int64})::Cvoid
+  @ccall libgalahad_quadruple_64.version_galahad_64(major::Ref{Int64}, minor::Ref{Int64}, patch::Ref{Int64})::Cvoid
   return VersionNumber(major[], minor[], patch[])
 end
